50f332a282d476a54f07c486f3879ba008166cb8
Rares Raulea <rares@graffino.com>
2023-09-12 22:09:49 +0300

    fix: update config examples

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 import { NativeAuthClient } from "./native.auth.client";
 
-export const origin = 'http://localhost:3000';
-export const address = "erd19rh30cq9964an8vj7qnj7gwaus90nv6020vxpu69ramwrn8yr78smteycl";
+export const origin = 'https://localhost:3000';
+export const address = "erd1...";
 export const client = new NativeAuthClient({
     origin,
     expirySeconds: 7 * 24 * 60 * 60,
