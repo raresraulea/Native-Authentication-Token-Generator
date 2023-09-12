55744505595d7a210e00ef0d408437a23c3c90da
Rares Raulea <rares@graffino.com>
2023-09-12 22:10:01 +0300

    fix: update signed message example

--- src/generateAccessToken.ts
+++ src/generateAccessToken.ts
@@ -1,6 +1,6 @@
 import { address, client } from './config';
 
-const signedMessageFromUtilsMvx = "7329f06b26c1fe3b96c739160447a25c174b6096bcdde44d55ff602e2e20c643bdfaebfb62d4b2579b80e64d7e18670e775fa270d4c1e19c3b561980c0000f08";
+const signedMessageFromUtilsMvx = "SIGNED_MESSAGE_GOES_HERE";
 
 void (async () => {
     const init = await client.initialize();
