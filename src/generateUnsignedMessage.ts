81da6834468d3a8d7dd4e42960b0026bc921e8f9
Rares Raulea <rares@graffino.com>
2023-09-12 22:10:26 +0300

    improvement: sync output message with docs

--- src/generateUnsignedMessage.ts
+++ src/generateUnsignedMessage.ts
@@ -5,6 +5,6 @@ void (async () => {
     const init = await client.initialize();
     const unsignedMessage = `${address}${init}`;
 
-    console.log("Sign this message using utils.multiversx.com:\n", unsignedMessage);
+    console.log("Sign the following message using utils.multiversx.com:\n", unsignedMessage);
 })();
 
