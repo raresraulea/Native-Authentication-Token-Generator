2f8504eb7c6d20698348fc9368111e3f26ff1a72
Rares Raulea <rares@graffino.com>
2023-09-13 00:29:56 +0300

    feat: combine steps with sync prompt in the console

--- src/generateAccessToken.ts
+++ src/generateAccessToken.ts
@@ -1,11 +1,39 @@
-import { address, client } from './config';
+import { address } from './config';
+import * as promptSync from 'prompt-sync';
+import { CustomNativeAuthClient } from './custom.native.auth.client';
+const emojic = require('emojic');
 
-const signedMessageFromUtilsMvx = "SIGNED_MESSAGE_GOES_HERE";
+const colors = require('colors');
+colors.enable();
 
 void (async () => {
-    const init = await client.initialize();
-    const accessToken = client.getToken(address, init, signedMessageFromUtilsMvx);
+   
+    const { clientInstance, init } = await CustomNativeAuthClient.build();
+
+    console.log(("🟡 Building Signable message..." as any).yellow);
+    const unsignedMessage = `${address}${init}`;
+
+    console.log("✅ Build was successful! Please sign the following message using utils.multiversx.com:\n", (unsignedMessage as any).blue);
     
-    console.log("This is your access token:\n", accessToken);
+    if (!clientInstance || CustomNativeAuthClient.getInit() === '' || !CustomNativeAuthClient.getInit()) {
+        console.log(emojic.thumbsUp + ("There was a problem with the Native Auth Client: " as any).green, {
+            clientInstance,
+            init: CustomNativeAuthClient.getInit(),
+        });
+        return;
+    }
+
+    const prompt = promptSync();
+    const signedMessageFromUtilsMvx = prompt("\n Paste the signed message here: ");
+
+    console.log(("🟡 Generating Access Token..." as any).yellow);
+
+    const accessToken = clientInstance.getToken(
+        address,
+        CustomNativeAuthClient.getInit(),
+        signedMessageFromUtilsMvx
+    );
+    
+    console.log(("✅ Generated token successfully! This is your access token:\n" as any).green + (accessToken as any).blue);
 })();
 
